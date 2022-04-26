005516b021fa1b324459c9a3ec49d095777f9408
Mateus Pereira Rodrigues <86198294+mateusmaiadb@users.noreply.github.com>
2022-04-26 15:45:11 -0300

    atualização

--- models/app.test.ts
+++ models/app.test.ts
@@ -1,15 +1,23 @@
-import { calcularTrocoComCedulas, calcularTroco, calcularMoedas } from "../src/app";
+import {  calcularTroco } from "../src/app";
 
 describe("Troco", () => {
-  // test("Teste troco", () => {
-  //   const resultado = calcularTroco(100.50, 100);
-  // });
-
-  test("Teste de calculo de troco com cedulas com 115 de troco ", () => {
-    const resultado = calcularTrocoComCedulas(15);
+  test("Quando a função calcularTroco for chamado com o valor pago de 100,50 e o valor do produto for 100 , deve retornar o resultado do troco de 0,50 centavos ", () => {
     
-    expect(resultado).toBe(15);
+    const valorPago = 100.50;
+    const valorCompra = 100;
+    const resultado = 0.50;
+    
+    const troco = calcularTroco(valorPago, valorCompra);
+
+    expect(troco).toBe(resultado);
+
   });
+
+  // test("Teste de calculo de troco com cedulas com 115 de troco ", () => {
+  //   const resultado = calcularTrocoComCedulas(115);
+    
+  //   expect(resultado).toBe(15);
+  // });
 
 //   test("Teste moedas", () => {
 //     const resultado = calcularMoedas(calcularTroco(100.49, 100));
--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,32 @@
 var cedulas: number[] = [100, 50, 20, 10, 5, 2];
 var moedas: number[] = [1, 0.5, 0.25, 0.1, 0.05];
 var troco: number;
-var valorPago = 100.49;
-var valorCompra = 100;
-
 
 export function calcularTroco(valorPago: number, valorCompra: number): number {
   troco = valorPago - valorCompra;
   return troco;
 }
 
-export function calcularTrocoComCedulas(troco: number): number {
+export function calcularTrocoComCedulasEMoedas(troco: number) {
+  let resultado: any = [];
+
   for (var i = 0; i < cedulas.length; i++) {
-    var qtdCedulas = Math.floor(troco / cedulas[i]);
-    troco = troco % cedulas[i];
-    console.log("Cédula de " + cedulas[i] + " quantidade: " + qtdCedulas);
+    var qtdCedulas = Math.floor(Math.floor(troco) / cedulas[i]);
+    if (qtdCedulas > 0) {
+      troco = troco % cedulas[i];
+      resultado.push(`Cédula de ${cedulas[i]}   quantidade: ${qtdCedulas}`);
+    }
   }
-  return troco;
-}
 
-// calcularTrocoComCedulas(calcularTroco(valorPago, valorCompra));
-
-export function calcularMoedas(troco: number): void {
   for (var i = 0; i < moedas.length; i++) {
     var qtdMoedas = Math.floor(troco / moedas[i]);
-    troco = troco % moedas[i];
-    troco = +troco.toFixed(2);
-    if (troco < 0.05) {
-      troco = 0.05;
+    if (qtdMoedas > 0) {
+      troco -= qtdMoedas * moedas[i];
+      troco = +troco.toFixed(2);
+      resultado.push(`Moeda de ${moedas[i]} quantidade: ${qtdMoedas}`);
     }
-    console.log(moedas[i] + ": " + qtdMoedas);
-    console.log(troco);
   }
-
-  //   console.log("Troco: " + troco);
+  return resultado;
 }
-calcularMoedas(calcularTroco(valorPago, valorCompra));
+
+console.log(calcularTrocoComCedulasEMoedas(calcularTroco(100, 50.5)));
